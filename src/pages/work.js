7382df90b962f012c67f0bf4f07f7e9440a43678
maxachis <maxachis@gmail.com>
2023-01-01 19:10:21 -0500

    Add custom accordion appearance

--- src/pages/work.js
+++ src/pages/work.js
@@ -4,23 +4,35 @@ import {AccordionDetails, AccordionSummary} from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 import Config from "./../config.js";
+import {styled} from "@mui/material/styles";
+
+const CustomAccordionSummary = styled(AccordionSummary)`
+  background-color: #d1dede;
+  border: black solid 1px;
+  z-index: 0;
+`;
+
+const CustomAccordion = styled(Accordion)`
+  z-index: 0;
+`;
+
 
 function JobAccordion(props) {
     return (
         <div>
 
-            <Accordion>
-                <AccordionSummary
+            <CustomAccordion>
+                <CustomAccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                 >
                     <Typography variant={"h5"}>{props.job.title} ({props.job.start_date} - {props.job.end_date})</Typography>
-                </AccordionSummary>
+                </CustomAccordionSummary>
                 <AccordionDetails>
                     <p><img style={{float: 'Right', padding: 1}} height={"100"} src={props.job.logo} alt="Company Logo"></img>
                         {props.job.content}
                     </p>
                 </AccordionDetails>
-            </Accordion>
+            </CustomAccordion>
         </div>
     )
 }
